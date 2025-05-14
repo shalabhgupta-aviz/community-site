46fef12b202c1440fba67c63d23150e6f42d1960
shalabhgupta <shalabh.gupta@aviznetworks.com>
2025-05-14 19:44:51 +0530

    fix not found issue - link

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function NotFound() {
   return (
-    <div className="h-screen flex items-center justify-center flex-col">
+    <div className="h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">404 — Page Not Found</h1>
       <p className="mt-4 text-center">
         Sorry, we couldn&apos;t find what you were looking for.
